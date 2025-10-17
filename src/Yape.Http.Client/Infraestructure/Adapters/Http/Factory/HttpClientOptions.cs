6ab1e3a45db476c889aa4d39b9754795c650a4a8
Leandro Tuttini <LTuttini@bcp.com.bo>
2025-10-17 10:35:00 -0400

    update readme

--- src/Yape.Http.Client/Infraestructure/Adapters/Http/Factory/HttpClientOptions.cs
+++ src/Yape.Http.Client/Infraestructure/Adapters/Http/Factory/HttpClientOptions.cs
@@ -1,10 +1,9 @@
 ﻿using Yape.Library.Http.Client.Domain.Port;
 
-namespace Yape.Library.Http.Client.Infraestructure.Adapters.Http
+namespace Yape.Library.Http.Client.Infraestructure.Adapters.Http;
+
+public class HttpClientOptions
 {
-    public class HttpClientOptions
-    {
-        public bool IncludeHeadersRequired { get; set; } = true;
-        public HttpErrorMapperBase? ErrorMapper { get; set; } = null;
-    }
+    public bool IncludeHeadersRequired { get; set; } = true;
+    public HttpErrorMapperBase? ErrorMapper { get; set; } = null;
 }
--- src/Yape.Http.Client/Infraestructure/Adapters/Http/HttpErrorMapperDefault.cs
+++ src/Yape.Http.Client/Infraestructure/Adapters/Http/HttpErrorMapperDefault.cs
@@ -1,60 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Polly.CircuitBreaker;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
 using Yape.Library.ErrorBuilder.Domain.Builder;
-using Yape.Library.Http.Client.Domain.Port;
 
 
-namespace Yape.Library.Http.Client.Infraestructure.Adapters.Http
+namespace Yape.Library.Http.Client.Infraestructure.Adapters.Http;
+
+public class HttpErrorMapperDefault : HttpErrorMapperBase
 {
-    public class HttpErrorMapperDefault : HttpErrorMapperBase
+
+    public HttpErrorMapperDefault(ILogger logger) : base(logger)
     {
-
-        public HttpErrorMapperDefault(ILogger logger) : base(logger)
-        {
-        }
-
-        public async override Task<bool> HttpRequestFailed(HttpResponseMessage response, HttpRequestException ex)
-        {
-            bool result = false;
-
-            if (response.Content.Headers.ContentLength == 0) // Manejar respuestas vacías 
-            {
-                return true;
-            }
-
-            ProblemDetails? problemDetails = null;
-            try
-            {
-                problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>(new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true,
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-
-                });
-            }
-            catch (JsonException)
-            {
-                result = true;
-            }
-
-            if (problemDetails != null)
-            {
-                throw new YapeGeneralExceptionBuilder()
-                {
-                    Status = (HttpStatusCode)problemDetails.Status.GetValueOrDefault(),
-                    ErrorCode = problemDetails.Type,
-                    Title = problemDetails.Title,
-                    Detail = problemDetails.Detail
-                };
-            }
-
-            return result;
-        }
-
-
     }
+
+    public async override Task<bool> HttpRequestFailed(HttpResponseMessage response, HttpRequestException ex)
+    {
+        bool result = false;
+
+        if (response.Content.Headers.ContentLength == 0) // Manejar respuestas vacías 
+        {
+            return true;
+        }
+
+        ProblemDetails? problemDetails = null;
+        try
+        {
+            problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>(new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+
+            });
+        }
+        catch (JsonException)
+        {
+            result = true;
+        }
+
+        if (problemDetails != null)
+        {
+            throw new YapeGeneralExceptionBuilder()
+            {
+                Status = (HttpStatusCode)problemDetails.Status.GetValueOrDefault(),
+                ErrorCode = problemDetails.Type,
+                Title = problemDetails.Title,
+                Detail = problemDetails.Detail,
+                Extensions = problemDetails.Extensions
+            };
+        }
+
+        return result;
+    }
+
+
 }
